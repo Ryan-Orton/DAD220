f85434a6d31b8aac1304f42facb01b9638843b1f
Ryan-Orton <ryanorton@bellsouth.net>
2025-02-01 20:13:30 -0500

    Update Module One Activity.sql
    Added comments

--- Module One Activity.sql
+++ Module One Activity.sql
@@ -1,10 +1,23 @@
+-- Create a new database named 'orton'
 CREATE DATABASE orton;
+
+-- Display all databases to verify the creation of the 'orton' database
 SHOW DATABASES;
+
+-- Select the 'orton' database for use
 USE orton;
 
+-- Create a new table named 'tb2' with a column 'user_id' of type VARCHAR(50)
 CREATE TABLE tb2 (user_id VARCHAR(50));
+
+-- Display all tables in the 'orton' database to verify that the 'tb2' table was created
 SHOW TABLES;
+
+-- Describe the structure of the 'tb2' table to show its columns and data types
 DESCRIBE tb2;
 
+-- Add a new column named 'newfield' of type VARCHAR(25) to the 'tb2' table
 ALTER TABLE tb2 ADD newfield VARCHAR(25);
+
+-- Describe the structure of the 'tb2' table again to verify that the new column has been added
 DESCRIBE tb2;
